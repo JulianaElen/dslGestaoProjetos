59293e5ae77a3f03f371bb72ed8ad2e0c8afcb18
JulianaElen <julianaesoliveira@gmail.com>
2024-11-19 18:05:49 -0300

    Linha de comandos numerada

--- dsl_interpreter.py
+++ dsl_interpreter.py
@@ -9,5 +9,17 @@ import re
 class DSL_interpreter(cmd.Cmd):
     intro="Bem-vindo à interface de linha de comando da DSL para Gestão de Projetos. Digite 'help' ou '?' para listar os comandos.\n"
 
+    def __init__(self):
+        super().__init__()
+        self.current_project = None
+        self.current_task_file = None
+        self.command_count = 1
+        self.prompt = f"1 > "
+
+    def precmd(self, line):
+        self.command_count += 1
+        self.prompt = f"{self.command_count} > "
+        return line
+
 if __name__ == "__main__":
     DSL_interpreter().cmdloop()
